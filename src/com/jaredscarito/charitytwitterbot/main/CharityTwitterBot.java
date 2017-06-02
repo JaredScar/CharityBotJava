333a32eb2c3b4e5976b464b66e499d666dba8a82
ZP4RKER <zp4rkercoc@gmail.com>
2017-06-02 14:50:18 +1000

    Fixed some small errors.

--- src/com/jaredscarito/charitytwitterbot/main/CharityTwitterBot.java
+++ src/com/jaredscarito/charitytwitterbot/main/CharityTwitterBot.java
@@ -13,83 +13,69 @@ import java.util.ArrayList;
  * Created by TheWolfBadger on 2/21/17.
  */
 public class CharityTwitterBot {
-        private static Twitter twitter;
-        private static File infoFile = new File("info.yml");
-        public static void main(String args[]) throws TwitterException {
-            if(!infoFile.exists()) {
-                try {
-                    infoFile.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            YamlConfiguration configY = YamlConfiguration.loadConfiguration(infoFile);
-            if(!configY.contains("Oauth")) {
-                //Set defaults up
-                configY.set("Oauth.ConsumerKey", "");
-                configY.set("Oauth.ConsumerSecret", "");
-                configY.set("Oauth.accesstoken", "");
-                configY.set("Oauth.accesstokensecret", "");
-                configY.set("Keywords", new ArrayList<>());
-                try {
-                    configY.save(infoFile);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            ConfigurationBuilder cb = new ConfigurationBuilder();
-            cb.setDebugEnabled(true);
-            cb.setOAuthConsumerKey(configY.getString("Oauth.ConsumerKey"));
-            cb.setOAuthConsumerSecret(configY.getString("Oauth.ConsumerSecret"));
-            cb.setOAuthAccessToken(configY.getString("Oauth.AccessToken"));
-            cb.setOAuthAccessTokenSecret(configY.getString("Oauth.AccessTokenSecret"));
-            Configuration conf = cb.build();
-            twitter = new TwitterFactory(conf).getInstance();
-            System.out.println("Running it");
-            final int[] tweetsInfo = {0, 100}; // Tweeted, AllowedPerHour
-            StatusListener listener = new StatusListener() {
+    private static Twitter twitter;
+    private static File infoFile = new File("info.yml");
+    public static void main(String args[]) throws TwitterException, IOException {
+        if(!infoFile.exists()) infoFile.createNewFile();
+
+        YamlConfiguration configY = YamlConfiguration.loadConfiguration(infoFile);
+        if(!configY.contains("Oauth")) {
+            //Set defaults up
+            configY.set("Oauth.ConsumerKey", "");
+            configY.set("Oauth.ConsumerSecret", "");
+            configY.set("Oauth.accesstoken", "");
+            configY.set("Oauth.accesstokensecret", "");
+            configY.set("Keywords", new ArrayList<>());
+            configY.save(infoFile);
+        }
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
+        cb.setOAuthConsumerKey(configY.getString("Oauth.ConsumerKey"));
+        cb.setOAuthConsumerSecret(configY.getString("Oauth.ConsumerSecret"));
+        cb.setOAuthAccessToken(configY.getString("Oauth.AccessToken"));
+        cb.setOAuthAccessTokenSecret(configY.getString("Oauth.AccessTokenSecret"));
+        Configuration conf = cb.build();
+        twitter = new TwitterFactory(conf).getInstance();
+        System.out.println("Running it");
+        final int[] tweetsInfo = {0, 100}; // Tweeted, AllowedPerHour
+        StatusListener listener = new StatusListener() {
 
             public void onStatus(Status status) {
                 try {
                     // System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-                    if(status.getUser().getId() !=twitter.getId()) {
-                        if (!status.isRetweet()) {
-                            String text = status.getText().toLowerCase();
-                            int retweetedAlready = 0;
-                            for(String keywords : configY.getStringList("Keywords")) {
-                                if(text.contains(keywords.toLowerCase()) && retweetedAlready == 0) {
-                                    if (!(status.getUserMentionEntities().length >= 2)) {
-                                        if (tweetsInfo[1] <= tweetsInfo[0]) {
-                                            System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-                                            //TODO Make this a randomized message
-                                            //StatusUpdate stat = new StatusUpdate("Thanks for the tag! @" + status.getUser().getScreenName());
-                                            //stat.setInReplyToStatusId(status.getId());
-                                            //twitter.updateStatus(stat);
-                                            twitter.createFavorite(status.getId());
-                                            twitter.retweetStatus(status.getId());
-                                            tweetsInfo[0]++;
-                                            twitter.createFriendship(status.getUser().getId());
-                                            retweetedAlready = 1;
-                                            //TODO Put this in a direct message received shit
-                                            /** /
-                                             twitter.sendDirectMessage(status.getUser().getId(),
-                                             "Hello there, I am trying to win this contest for charity! If I win please just message me back with 'won' " +
-                                             "and I will let you know the details to get the package to me! Thanks!");
-                                             /**/
-                                        } else {
-                                            try {
-                                                tweetsInfo[0] = 0;
-                                                Thread.sleep(1000 * 60 * 60);
-                                            } catch (InterruptedException e) {
-                                                e.printStackTrace();
-                                            }
-                                        }
-                                    }
+                    if(status.getUser().getId() != twitter.getId()) return;
+                    if (!status.isRetweet()) return;
+
+                    String text = status.getText().toLowerCase();
+                    boolean retweetedAlready = false;
+                    for(String keywords : configY.getStringList("Keywords")) {
+                        if(text.contains(keywords.toLowerCase()) && !retweetedAlready) {
+                            if (status.getUserMentionEntities().length < 3) {
+                                if (tweetsInfo[0] < tweetsInfo[1]) {
+                                    System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+                                    //TODO Make this a randomized message
+                                    //StatusUpdate stat = new StatusUpdate("Thanks for the tag! @" + status.getUser().getScreenName());
+                                    //stat.setInReplyToStatusId(status.getId());
+                                    //twitter.updateStatus(stat);
+                                    twitter.createFavorite(status.getId());
+                                    twitter.retweetStatus(status.getId());
+                                    tweetsInfo[0]++;
+                                    twitter.createFriendship(status.getUser().getId());
+                                    retweetedAlready = true;
+                                    //TODO Put this in a direct message received shit
+                                    /** /
+                                     twitter.sendDirectMessage(status.getUser().getId(),
+                                     "Hello there, I am trying to win this contest for charity! If I win please just message me back with 'won' " +
+                                     "and I will let you know the details to get the package to me! Thanks!");
+                                     /**/
+                                } else {
+                                    tweetsInfo[0] = 0;
+                                    Thread.sleep(1000 * 60 * 60);
                                 }
                             }
                         }
                     }
-                } catch (TwitterException e) {
+                } catch (TwitterException|InterruptedException e) {
                     e.printStackTrace();
                 }
             }
